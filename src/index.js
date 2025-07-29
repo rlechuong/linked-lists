95ec737829dadd3ae9296d3d40baf54c655e6549
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-07-29 16:20:39 -0400

    Add prepend, size, head, tail functions.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,48 @@ class LinkedList {
       current.nextNode = newNode;
     }
   }
+
+  prepend(value) {
+    const newNode = new Node(value, null);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      const temp = this.head;
+      this.head = newNode;
+      newNode.nextNode = temp;
+    }
+  }
+
+  size() {
+    let counter = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      counter += 1;
+      current = current.nextNode;
+    }
+
+    return counter;
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    if (this.head === null) {
+      return null;
+    }
+
+    let current = this.head;
+
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+
+    return current;
+  }
 }
 
 class Node {
@@ -32,5 +74,10 @@ class Node {
 const testLinkedList = new LinkedList();
 testLinkedList.append("Dog");
 testLinkedList.append("Cat");
+testLinkedList.prepend("Dragon");
 
 console.log(testLinkedList);
+console.log(testLinkedList.size());
+console.log(testLinkedList.head);
+console.log("The Tail is...");
+console.log(testLinkedList.tail());
